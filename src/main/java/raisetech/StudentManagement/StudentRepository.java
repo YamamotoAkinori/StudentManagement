255a6a7ccefcc16c622af2909b983f1c5bcbf8aa
Gloria <l2202670970as@gmail.com>
2025-01-18 13:56:48 +0900

    最初のコミット

---
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -0,0 +1,16 @@
+package raisetech.StudentManagement;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import java.util.List;
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT * FROM students")
+  List<Student> search();
+
+  @Select("SELECT * FROM students_courses")
+  List<StudentsCourses> searchStudentsCourses();
+
+}
---
+++ src/main/java/raisetech/StudentManagement/StudentsCourses.java
@@ -0,0 +1,16 @@
+package raisetech.StudentManagement;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudentsCourses {
+
+  private String id;
+  private String studentID;
+  private String courseName;
+  private String courseStartAt;
+  private String courseEndAt;
+
+}
