6f989bb4d03feb6a5ebc8da79cd65e1e885bea24
Gloria <l2202670970as@gmail.com>
2025-05-08 16:31:58 +0900

    リポジトリのテスト追加_修正

--- src/test/java/raisetech/StudentManagement/repository/StudentRepositoryTest.java
+++ src/test/java/raisetech/StudentManagement/repository/StudentRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentCourse;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +18,28 @@ class StudentRepositoryTest {
   void 受講生の全件検索が行えること() {
     List<Student> actual = sut.search();
     assertThat(actual.size()).isEqualTo(5);
+  }
+
+  @Test
+  void 受講生IDでの検索が行えること() {
+    Student actual = sut.searchStudent("1");
+
+    assertThat(actual).isNotNull();
+    assertThat(actual.getName()).isEqualTo("山田太郎"); // data.sqlで指定
+  }
+
+  @Test
+  void 受講生コース情報の全件検索が行えること() {
+    List<StudentCourse> actual = sut.searchStudentCourseList();
+    assertThat(actual.size()).isEqualTo(5);
+  }
+
+  @Test
+  void 受講生IDに紐づく受講生コース情報の検索が行えること() {
+    List<StudentCourse> actual = sut.searchStudentCourse("5");
+
+    assertThat(actual).hasSize(1);
+    assertThat(actual.get(0).getCourseName()).isEqualTo("Javaコース");
   }
 
   @Test
@@ -37,6 +60,46 @@ class StudentRepositoryTest {
     List<Student> actual = sut.search();
 
     assertThat(actual.size()).isEqualTo(6);
+
+    assertThat(actual)
+            .extracting(Student::getName)
+            .contains("江並公史");
   }
 
+  @Test
+  void 受講生コース情報の登録が行えること() {
+    StudentCourse studentCourse = new StudentCourse();
+    studentCourse.setStudentId("1");
+    studentCourse.setCourseName("Spring入門コース");
+
+    sut.registerStudentCourse(studentCourse);
+
+    List<StudentCourse> actual = sut.searchStudentCourse("1");
+    assertThat(actual)
+            .extracting(StudentCourse::getCourseName)
+            .contains("Spring入門コース");
+  }
+
+  @Test
+  void 受講生情報の更新が行えること() {
+    Student student = sut.searchStudent("1");
+    student.setName("田中一郎");
+
+    sut.updateStudent(student);
+
+    Student updated = sut.searchStudent("1");
+    assertThat(updated.getName()).isEqualTo("田中一郎");
+  }
+
+  @Test
+  void 受講生コース情報のコース名の更新が行えること() {
+    List<StudentCourse> courses = sut.searchStudentCourse("1");
+    StudentCourse course = courses.get(0);
+    course.setCourseName("AWSコース");
+
+    sut.updateStudentCourse(course);
+
+    List<StudentCourse> updated = sut.searchStudentCourse("1");
+    assertThat(updated.get(0).getCourseName()).isEqualTo("AWSコース");
+  }
 }
